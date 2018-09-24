9ac4120046b5ca2b1b24f5db251d671e0ad6513e
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-09-24 12:09:06 +0300

    configure database

--- config.py
+++ config.py
@@ -6,9 +6,12 @@ class ProdConfig(Config):
     pass
 
 class TestConfig(Config):
-    pass
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:moringa@localhost/portfolio_test'
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:moringa@localhost/portfolio'
     DEBUG = True
 
 config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
