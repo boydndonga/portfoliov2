12b7946053b08ecbb77d5f2e0a3bbdba660c9d5d
boyd ndonga <boydbugle@gmail.com>
2018-09-25 12:46:51 +0300

    create error handler for routes

---
+++ tests/test_user_model.py
@@ -0,0 +1,11 @@
+import unittest
+from flask import current_app
+from app import create_app
+
+
+class UserModelTESTCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client
