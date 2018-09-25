9158da3ee99beaa548106444594254e166b4c931
boyd ndonga <boydbugle@gmail.com>
2018-09-25 20:43:44 +0300

    resolve bug

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import unittest
 from flask_script import Manager,Server
 from app import create_app,db
-from  flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 from app.models import User
 
 app = create_app('production')
