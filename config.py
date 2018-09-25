e29677692fd5bc2370b06cb6b19f9fe0d99211cc
boyd ndonga <boydbugle@gmail.com>
2018-09-25 20:04:14 +0300

    update configuration environments

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import os
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CSRF_ENABLED = False
-    SECRET_KEY=os.urandom(24)
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DB')
-    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     TESTING = True
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DB')
 
-   
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DB')
+    DEBUG = True
 
-config_options ={"production":ProdConfig, "testing":TestConfig}
+config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
 
