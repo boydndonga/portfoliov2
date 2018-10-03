ebea1dd2deb750aa9d7dddbf097b72783628024c
boyd ndonga <boydbugle@gmail.com>
2018-10-04 01:07:14 +0300

    create form category form

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,24 @@
 from flask import render_template, request, redirect, url_for,abort
 from . import main
 from .. import db
+from app.models import Category
+from flask_login import login_required, current_user
+from .forms import CategoryForm
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/new_category')
+@login_required
+def new_category():
+    form = CategoryForm()
+    if form.validate_on_submit():
+        name = form.name.data
+
+        new_category=Category(name=name)
+
+        new_category.save_category()
+        return redirect(url_for('.new_category'))
+    
+    return render_template('main/new_review.html')
